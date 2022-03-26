86260649f2931ccd06657a728d450cce7b0148b0
Dylan-HYF <68765633+Dylan-HYF@users.noreply.github.com>
2022-03-26 16:16:54 -0400

    chatbot skeleton finished

--- src/routes/Bot.js
+++ src/routes/Bot.js
@@ -1,5 +1,38 @@
+import { BsThreeDots } from "react-icons/bs";
+
 export const Bot = () => {
   return (
-    <div>Bot!</div>
+    <div className="d-flex flex-column vh-100">
+      <section className="h-75 mb-2 overflow-auto w-75 d-flex flex-column align-items-center mt-5 mx-auto">
+        <p className="w-75 fs-3 align-self-start chat-text">
+          “James, the spirits told me you were looking for me. I'm Chatbot, I'll be your guide. Would you like me to read your fortunes?”
+        </p>
+        <p className="w-75 fs-3 align-self-end chat-text text-light text-end">
+          “Yes please.”
+        </p>
+        <p className="w-75 fs-3 align-self-start chat-text">
+          “James, the spirits told me you were looking for me. I'm Chatbot, I'll be your guide. Would you like me to read your fortunes?”
+        </p>
+        <p className="w-75 fs-3 align-self-start chat-text">
+          “James, the spirits told me you were looking for me. I'm Chatbot, I'll be your guide. Would you like me to read your fortunes?”
+        </p>
+        <p className="w-75 fs-3 align-self-start chat-text">
+          “James, the spirits told me you were looking for me. I'm Chatbot, I'll be your guide. Would you like me to read your fortunes?”
+        </p>
+        <p className="w-75 fs-3 align-self-start chat-text">
+          “James, the spirits told me you were looking for me. I'm Chatbot, I'll be your guide. Would you like me to read your fortunes?”
+        </p>
+        <p className="w-75 fs-3 align-self-start chat-text">
+          “James, the spirits told me you were looking for me. I'm Chatbot, I'll be your guide. Would you like me to read your fortunes?”
+        </p>
+        <p className="w-75 fs-3 align-self-start chat-text">
+          “James, the spirits told me you were looking for me. I'm Chatbot, I'll be your guide. Would you like me to read your fortunes?”
+        </p>
+      </section>
+      <div className="w-75 align-self-center">
+        <BsThreeDots className="display-5" />
+        <input type="text" className="form-control rounded-pill" placeholder="chat" />
+      </div>
+    </div>
   )
 }
--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,7 +1,10 @@
 export const Login = () => {
   return (
-    <div>
-      login!
-    </div>
+    <section className="vh-100 text-center d-flex flex-column justify-content-center align-items-center section-center">
+      <p className="mb-0">Welcome,</p>
+      <h1 className="display-3 mb-5">Create your account</h1>
+      <button type="button" className="btn btn-primary mt-3">Sign in with Google</button>
+      <button type="button" className="btn btn-primary mt-3">Sign in with Facebook</button>
+    </section>
   )
 }
