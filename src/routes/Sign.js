7f0396e5831948338da5ba7ffafde4169a878f70
Dylan-HYF <68765633+Dylan-HYF@users.noreply.github.com>
2022-04-04 08:53:31 -0400

    sign page responsive

--- src/routes/Sign.js
+++ src/routes/Sign.js
@@ -9,13 +9,12 @@ export const Sign = ({ user }) => {
   const userSign = user.sign
   return (
     <section className="vh-100 text-center d-flex flex-column justify-content-center align-items-center section-center">
-      <img src={images[`${userSign && userSign.toLowerCase()}.png`]} alt={userSign} className="img-fluid w-25 mb-4" />
+      <img src={images[`${userSign && userSign.toLowerCase()}.png`]} alt={userSign} className="img-fluid w-25 mb-4 sign-pic" />
       <p className="mb-0">{user.displayName}, it appears that you are a </p>
       <h1 className="display-3 mb-5">{userSign}</h1>
-      <div>
+      <div className="sign-btns">
         <Link to="/prediction">
           <button type="button" className="btn btn-primary narrow">Prediction</button>
-
         </Link>
         <Link to="/analysis">
           <button type="button" className="btn btn-primary narrow">Analysis</button>
