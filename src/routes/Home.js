32f8b695b3f5da821eedb024a7f4880da2ebc60a
Dylan-HYF <68765633+Dylan-HYF@users.noreply.github.com>
2022-04-08 11:14:11 -0400

    home page finished

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,26 @@
-import zodiac from '../assets/image 4.png'
-import { Link } from 'react-router-dom'
 
+import { Link } from 'react-router-dom'
+import { importAll } from '../utils/importAll'
+const images = importAll(require.context('../assets/zodiac', false, /\.(png|jpe?g|svg)$/));
+
+const imagesTitle = Object.entries(images).map(x => [x[0].split('.')[0], x[1]])
+console.log(imagesTitle)
 export const Home = () => {
   return (
     <main>
-      <Link to="preview">
-        <img src={zodiac} alt="zodiac" className="img-fluid" />
-      </Link>
-      <p className="fs-2">
+
+      <div className="preview">
+        {imagesTitle.map(x => (
+          <Link key={x[0]} state={{ sign: x[0] }} to="preview" className="preview-link">
+            <div className="preview-img">
+              <img src={x[1]} alt={x[0]} />
+
+            </div>
+            <h4 className="fs-5">{x[0]}</h4>
+          </Link>
+        ))}
+      </div>
+      <p className="fs-1 my-5">
         Gaze upon the stars to<br />
         see your future
       </p>
--- src/routes/ZodiacPreview.js
+++ src/routes/ZodiacPreview.js
@@ -1,19 +1,51 @@
-import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai"
-import zodiac from '../assets/image 1.png'
+import { AiOutlineLeft } from "react-icons/ai"
+import { useLocation, useNavigate } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { importAll } from '../utils/importAll'
+const images = importAll(require.context('../assets/zodiac image', false, /\.(png|jpe?g|svg)$/));
 export const ZodiacPreview = () => {
-  return (
-    <main className="preview">
-      <AiOutlineLeft className="arrow" />
-      <section>
-        <img src={zodiac} alt="zodiac" className="img-fluid" />
-        <h1>Libra</h1>
-        <p>September 23 - October 22</p>
-        <p className="mt-5 mx-auto zodiac-detail">
-          Librans are extroverted, cosy, and friendly people. Librans, like the Scales that symbolise the sign, are often concerned with attaining balance, harmony, peace, and justice in the world. With their vast stores of charm, intelligence, frankness, persuasion, and seamless connectivity, they are well-equipped to do so.
-        </p>
-      </section>
-      <AiOutlineRight className="arrow" />
+  const navigate = useNavigate()
+  const location = useLocation()
+  const zodiacSign = location.state?.sign
+  useEffect(() => {
+    if (!zodiacSign) {
+      return navigate("/");
+    }
+  }, [zodiacSign, navigate])
+  const [analysis, setAnalysis] = useState({})
+  useEffect(() => {
+    let isMounted = true
+    const fetchAnalysis = async () => {
+      const res = await fetch(`http://localhost:8000/user/${zodiacSign[0]}${zodiacSign.slice(1).toLowerCase()}`)
+      const data = await res.json()
+      console.log(data)
+      if (isMounted) setAnalysis(data)
+    }
+    if (zodiacSign) {
+      fetchAnalysis().catch(err => console.log(err))
+    }
+    return () => { isMounted = false }
+  }, [zodiacSign])
 
-    </main>
+
+  return (
+    <section className="px-4">
+      <button onClick={() => window.history.back()} type="button" className="btn btn-link text-decoration-none">
+        <AiOutlineLeft className="little-arrow" />
+        <span className="align-middle fs-5 ms-2">Back</span>
+      </button>
+      <main className="overview">
+
+        <section>
+          <img src={images[`${zodiacSign ? zodiacSign.toLowerCase() : 'libra'}.png`]} alt={zodiacSign || 'libra'} className="img-fluid analysis-sign" />
+          <h1>{analysis.sign}</h1>
+          <p>{analysis.date}</p>
+          <p className="mt-5 mx-auto zodiac-detail">
+            {analysis.overview}
+          </p>
+        </section>
+
+      </main>
+    </section>
   )
 }
